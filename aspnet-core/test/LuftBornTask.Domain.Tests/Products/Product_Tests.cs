258a6f5cc34278a098bf18ee8fb10be7289524e7
Islam.Zakaria <Islam.Zakaria@GGSOPR9589LAP>
2026-02-11 20:27:57 +0200

    feat: add domain entity product test

---
+++ aspnet-core/test/LuftBornTask.Domain.Tests/Products/Product_Tests.cs
@@ -0,0 +1,66 @@
+using System;
+using Shouldly;
+using Xunit;
+
+namespace LuftBornTask.Products;
+
+public class Product_Tests : LuftBornTaskDomainTestBase<LuftBornTaskDomainTestModule>
+{
+    [Fact]
+    public void Should_Set_Name()
+    {
+        // Arrange
+        var product = new Product(Guid.NewGuid(), "Test Product", 10, "Description");
+
+        // Act
+        product.SetName("New Name");
+
+        // Assert
+        product.Name.ShouldBe("New Name");
+    }
+
+    [Fact]
+    public void Should_Set_Price()
+    {
+        // Arrange
+        var product = new Product(Guid.NewGuid(), "Test Product", 10, "Description");
+
+        // Act
+        product.SetPrice(20);
+
+        // Assert
+        product.Price.ShouldBe(20);
+    }
+
+    [Fact]
+    public void Should_Set_Description()
+    {
+        // Arrange
+        var product = new Product(Guid.NewGuid(), "Test Product", 10, "Description");
+
+        // Act
+        product.SetDescription("New Description");
+
+        // Assert
+        product.Description.ShouldBe("New Description");
+    }
+
+    [Fact]
+    public void Should_Create_Product()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var name = "Test Product";
+        var price = 10.5f;
+        var description = "Test Description";
+
+        // Act
+        var product = new Product(id, name, price, description);
+
+        // Assert
+        product.Id.ShouldBe(id);
+        product.Name.ShouldBe(name);
+        product.Price.ShouldBe(price);
+        product.Description.ShouldBe(description);
+    }
+}
