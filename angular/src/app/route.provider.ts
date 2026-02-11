b0fa3a81d89000538c687f01aafbd7982085027e
Islam Mohamed <eslamzakaria293@gmail.com>
2026-02-11 19:28:16 +0200

    feat: add path for products

--- angular/src/app/route.provider.ts
+++ angular/src/app/route.provider.ts
@@ -15,6 +15,13 @@ function configureRoutes(routesService: RoutesService) {
         order: 1,
         layout: eLayoutType.application,
       },
+      {
+        path: '/products',
+        name: '::Menu:Products',
+        iconClass: 'fas fa-box',
+        order: 2,
+        layout: eLayoutType.application,
+      },
     ]);
   };
 }
