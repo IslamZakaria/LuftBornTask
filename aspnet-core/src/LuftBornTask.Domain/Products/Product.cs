8247f9c7a1401ea20e925723db2a8577c4fe591a
Islam Mohamed <eslamzakaria293@gmail.com>
2026-02-11 19:20:11 +0200

    refactor: update roduct domain to be rich

--- aspnet-core/src/LuftBornTask.Domain/Products/Product.cs
+++ aspnet-core/src/LuftBornTask.Domain/Products/Product.cs
@@ -13,11 +13,45 @@ namespace LuftBornTask.Products
         {
         }
 
-        public Product(Guid id, string name, decimal price, string description)
+        public Product(Guid id, string name, decimal price, string description) : base(id)
         {
-            Id = id;
+            SetName(name);
+            SetPrice(price);
+            SetDescription(description);
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Product name cannot be empty", nameof(name));
+            }
+
+            if (name.Length > 128)
+            {
+                throw new ArgumentException("Product name cannot be longer than 128 characters", nameof(name));
+            }
+
             Name = name;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentException("Price cannot be negative", nameof(price));
+            }
+
             Price = price;
+        }
+
+        public void SetDescription(string description)
+        {
+            if (description != null && description.Length > 500)
+            {
+                throw new ArgumentException("Description cannot be longer than 500 characters", nameof(description));
+            }
+
             Description = description;
         }
     }
